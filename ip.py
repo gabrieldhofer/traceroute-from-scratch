4a80e8afb05ec8812d3a25f87e558d50a13f1ad7
Gabriel Hofer <leirbagabriel468@gmail.com>
2023-11-10 14:00:27 -0600

    readme.txt

--- ip.py
+++ ip.py
@@ -1,23 +1,16 @@
-
 class IP:
-
   def __init__(self): 
     self.data = [None] * 32
     self.options = [None] * 32
     self.destination_ip_addr = [None] * 32
     self.source_ip_addr = [None] * 32
-
     self.ttl = [None] * 8
     self.protocol = [None] * 8
     self.checksum = [None] * 16
-
     self.identification = [None] * 16
     self.flags = [None] * 3
     self.fragment_offset = [None] * 13
-
     self.version = [None] * 4
     self.header_length = [None] * 4
     self.type_of_service = [None] * 8
     self.total_length = [None] * 16
-
-
---
+++ readme.txt
@@ -0,0 +1,7 @@
+FAQ
+==========================================================
+
+Q:  What? 
+A:  Translating parts of "TCP/IP Illustrated Volume 1" 
+    by W. Richard Stevens into code (Python)
+
