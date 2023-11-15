182f8a398c1eeec240a937b39ab9f72ab72d61f0
Gabriel Hofer <leirbagabriel468@gmail.com>
2023-11-15 01:37:58 -0600

    update

--- traceroute.sh
+++ traceroute.sh
@@ -4,15 +4,16 @@ TTL=1
 while true;
 do
   RESPONSE=$(ping -c 1 -t "${TTL}" google.com)
-  IP=$(echo "${RESPONSE}" | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}) icmp_seq')
-  echo $IP | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'
+  IP=$(
+    echo "${RESPONSE}" | 
+    grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}) icmp_seq' | 
+    grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
   if [[ $RESPONSE == *"Time to live"* ]]; then
-    echo "TTL: $TTL";
+    echo "Hop #${TTL}: IP=${IP}";
   else
-    echo "NOPE"
+    echo "DONE"
     break
   fi
   sleep 1
   TTL=$((TTL + 1))
 done
-
