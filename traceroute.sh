3a83b3cdd553e5cb653000abad4819a263286cbf
Gabriel Hofer <leirbagabriel468@gmail.com>
2023-11-15 01:40:38 -0600

    update

--- traceroute.sh
+++ traceroute.sh
@@ -3,17 +3,17 @@
 TTL=1
 while true;
 do
-  RESPONSE=$(ping -c 1 -t "${TTL}" google.com)
-  IP=$(
-    echo "${RESPONSE}" | 
-    grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}) icmp_seq' | 
-    grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
-  if [[ $RESPONSE == *"Time to live"* ]]; then
-    echo "Hop #${TTL}: IP=${IP}";
-  else
-    echo "DONE"
+    RESPONSE=$(ping -c 1 -t "${TTL}" google.com)
+    IP=$(
+        echo "${RESPONSE}" | 
+        grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}) icmp_seq' | 
+        grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
+    if [[ $RESPONSE == *"Time to live"* ]]; then
+        echo "Hop #${TTL}: IP=${IP}";
+    else
+        echo "DONE"
     break
-  fi
-  sleep 1
-  TTL=$((TTL + 1))
+    fi
+    sleep 1
+    TTL=$((TTL + 1))
 done
